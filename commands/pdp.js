bae72ed76ed17a0ab94763c4995970fa3c2207ba
Anne-Gaëlle Schall <anne.gaelle.schall@eisti.fr>
2018-04-08 15:25:10 +0200

    Commande hello et pdp

---
+++ commands/pdp.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: 'pdp',
+    usage: '-pdp <user>',
+    description: 'Affiche la photo de profil de l\'utilisateur',
+    execute(message, args) {
+        if (!message.mentions.users.size) {
+			return message.channel.send(`Ta photo de profil: ${message.author.displayAvatarURL}`);
+		}
+
+		const avatarList = message.mentions.users.map(user => {
+			return `Photo de profil de ${user.username} : ${user.displayAvatarURL}`;
+		});
+
+		message.channel.send(avatarList);
+    },
+};
