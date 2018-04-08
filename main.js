a5092b5e9b8cbe291d2aff9dd2625ff25053ab86
Anne-Gaëlle Schall <anne.gaelle.schall@eisti.fr>
2018-04-08 11:25:51 +0200

    Ajout de fichiers, main, packages ...

---
+++ main.js
@@ -0,0 +1,25 @@
+// require the discord.js module
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+
+// create a new Discord client
+const client = new Discord.Client();
+
+// when the client is ready, run this code
+// this event will trigger whenever your bot:
+// - finishes logging in
+// - reconnects after disconnecting
+client.on('ready', () => {
+    console.log('Ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+	    // send back "Pong." to the channel the message was sent in
+	    message.channel.send('Pong.');
+	}
+});
+
+
+// login to Discord with your app's token
+client.login(token);
