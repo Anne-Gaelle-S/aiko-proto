8e73bb627430892c66155cabae9bac6594215783
Anne-Gaëlle Schall <anne.gaelle.schall@eisti.fr>
2018-04-09 18:43:11 +0200

    Maj des commandes

--- commands/hello.js
+++ commands/hello.js
@@ -1,32 +1,20 @@
 const { prefix } = require('../config.json');
 module.exports = {
     name: 'hello',
-    aliases: ['coucou', 'salut', 'plop', 'hoy', 'lu', 'hi', 'bonjour', 'hey', 'plop', 'hola', 'konnichiwa', 'ohayo'],
+    aliases: ['coucou', 'salut', 'plop', 'hoy', 'lu', 'hi', 'bonjour', 'hey', 'plop', 
+    'hola', 'konnichiwa', 'ohayo', 'halo', 'gutentag'],
     usage: ' ',
     description: 'hello!',
     execute(message, args) {
-    	alea = parseInt(Math.random() * 4);
-    	//console.log("Alea: "+alea);
-    	switch (alea) {
-    		case 0 : 
-    			message.channel.send(`Bonjour ${message.author}`);
-    			break;
+    	let alea = parseInt(Math.random() * 5);
 
-    		case 1 : 
-    			message.channel.send('Coucou.');
-    			break;
+    	let possibilites = [`Bonjour ${message.author}.`];
+    	possibilites.push('Coucou.');
+    	possibilites.push('Konnichiwa mina-san !');
+    	possibilites.push(`Salut ${message.author}.`);
+    	possibilites.push('...');
 
-    		case 2 : 
-    			message.channel.send('Konnichiwa mina-san !');
-    			break;
-
-    		case 3 :
-    			message.channel.send(`Salut ${message.author}`);
-    			break;
-
-    		default : 
-    			message.channel.send('Hello.');
-    	}
+    	message.channel.send(possibilites[alea]);
         
     },
 };
---
+++ commands/rip.js
@@ -0,0 +1,30 @@
+const { prefix } = require('../config.json');
+module.exports = {
+    name: 'rip',
+    aliases: ['rep'],
+    usage: `<user>`,
+    description: 'rip',
+    execute(message, args) {
+        let alea = parseInt(Math.random() * 3); 
+        let rare = parseInt(Math.random() * 100);
+
+        let messageRip = [`Requiescat in pace... `];
+        messageRip.push(`Reste en paix... `);
+        messageRip.push(`Rest in peace... `);
+       
+        if (!message.mentions.users.size) {
+            return message.channel.send(messageRip[alea]+` 🙏 💐 <:cercueil:432934894071709709>`);
+        }
+
+        const avatarList = message.mentions.users.map(user => {
+            let msg = `${messageRip[alea]}${user.username}. 🙏 💐 <:cercueil:432934894071709709>`;
+            if(rare == 0){
+                msg+=` ............. ou pas 🖕`;
+            } 
+            return msg;
+            
+        });
+
+        message.channel.send(avatarList);
+    },
+};
