30105537fa30fff89db6b5cec33019f2d11239df
Krysthalia <38167520+Krysthalia@users.noreply.github.com>
2018-04-10 23:42:05 +0200

    Update enfance.js

--- commands/enfance.js
+++ commands/enfance.js
@@ -6,14 +6,16 @@ module.exports = {
     execute(message, args) {
       let alea = parseInt(Math.random() * 5);
       
-    	let possibilites = [`https://raw.githubusercontent.com/Krysthalia/aiko-proto/master/img/0.gif`];
-    	possibilites.push('https://raw.githubusercontent.com/Krysthalia/aiko-proto/master/img/1.gif');      
-    	possibilites.push('https://raw.githubusercontent.com/Krysthalia/aiko-proto/master/img/2.gif');
-    	possibilites.push('https://raw.githubusercontent.com/Krysthalia/aiko-proto/master/img/3.gif');
-    	possibilites.push('https://raw.githubusercontent.com/Krysthalia/aiko-proto/master/img/4.gif');
+    	let possibilites = [['Level up!',`https://raw.githubusercontent.com/Krysthalia/aiko-proto/master/img/0.gif`]];
+    	possibilites.push(['C\'est donc à ça que les jambes servent ... ', 
+                           'https://raw.githubusercontent.com/Krysthalia/aiko-proto/master/img/1.gif']);      
+    	possibilites.push(['Miam! ...', 'https://raw.githubusercontent.com/Krysthalia/aiko-proto/master/img/2.gif']);
+    	possibilites.push(['I\'m so high man','https://raw.githubusercontent.com/Krysthalia/aiko-proto/master/img/3.gif']);
+    	possibilites.push(['Fin de l\'histoire!','https://raw.githubusercontent.com/Krysthalia/aiko-proto/master/img/4.gif']);
 
-      message.channel.send('', {
-              file : possibilites[alea]
+        let res = possibilites[alea];
+      message.channel.send(res[0], {
+              file : res[1]
           });
 
       },
