b50e09bbf3fbdfb43b9a7925c96c6a217e3d5e4a
Anne-Gaëlle Schall <anne.gaelle.schall@eisti.fr>
2018-04-16 11:37:39 +0200

    Un peu plus zoli gif.js

--- commands/gif.js
+++ commands/gif.js
@@ -18,7 +18,7 @@ module.exports = {
                            'https://raw.githubusercontent.com/Krysthalia/aiko-proto/master/img/aiko-ohayo.gif']);
     	possibilites.push(['C\'était trop simple!',
                            'https://raw.githubusercontent.com/Krysthalia/aiko-proto/master/img/ez.gif']);
-    	possibilites.push(['Elle a souvent des idées à la con...',
+    	possibilites.push(['Elle entree souvent des idées à la con...',
                            'https://raw.githubusercontent.com/Krysthalia/aiko-proto/master/img/AG.gif']);
     	possibilites.push(['Shigoto, shigoto ! ',
                            'https://raw.githubusercontent.com/Krysthalia/aiko-proto/master/img/TrioWork.gif']);
@@ -32,93 +32,47 @@ module.exports = {
                 file : res[1]
               });
           } else {
-              let a = args[0].toUpperCase();
-              if( a == 'LEA' || a == 'KAINNALY') {
-                       let alea = parseInt(Math.random() * 2);
-                       let res = possibilites[alea];
-                       message.channel.send(res[0], {
-                        file : res[1]
-                      });
-              } else if ( a == 'NICO' || a == 'NICOLAS' || a == 'KORNAKH') {
-                    let res = possibilites[2];
-                    message.channel.send(res[0], {
-                        file : res[1]
-                      });
-              } else if ( a == 'GAEL' || a == 'ZORG') {
-                    let res = possibilites[3];
-                    message.channel.send(res[0], {
-                        file : res[1]
-                      });
-              } else if ( a == 'ELIO' || a == 'SINISTAG') {
-                    let res = possibilites[4];
-                    message.channel.send(res[0], {
-                        file : res[1]
-                      });
-              }  else if ( a == 'OHAYO' || a == 'KONNICHIWA' || a == 'KONBAWA') {
-                    let res = possibilites[5];
-                    message.channel.send(res[0], {
-                        file : res[1]
-                      });
-              } else if ( a == 'EZ' || a == 'SIMPLE' || a == 'BASIQUE') {
-                    let res = possibilites[6];
-                    message.channel.send(res[0], {
-                        file : res[1]
-                      });
-              } else if ( a == 'AG' || a == 'KRYSTHALIA' || a == 'ANNE-GAELLE') {
-                    let res = possibilites[7];
-                    message.channel.send(res[0], {
-                        file : res[1]
-                      });
-              } else {
-                 let alea = parseInt(Math.random() * 9);
-                 let res = possibilites[alea];
-                 message.channel.send(res[0], {
-                    file : res[1]
-                  });
-              }
+              let entree = args[0].toUpperCase();
+              verifArg(possibilites, entree);
           }
+      } else {
+          const avatarList = message.mentions.users.map(user => {
+              let p = user.username.toUpperCase();
+              verifArg(possibilites, p);
+          });
       }
-        const avatarList = message.mentions.users.map(user => {
-            let p = user.username.toUpperCase();
-            if( p == 'KAINNALY') {
-                       let alea = parseInt(Math.random() * 2);
-                       let res = possibilites[alea];
-                       message.channel.send(res[0], {
-                        file : res[1]
-                      });
-              } else if ( p == 'KORNAKH') {
-                    let res = possibilites[2];
-                    message.channel.send(res[0], {
-                        file : res[1]
-                      });
-              } else if ( p == 'ZORG') {
-                    let res = possibilites[3];
-                    message.channel.send(res[0], {
-                        file : res[1]
-                      });
-              } else if ( p == 'SINISTAG') {
-                    let res = possibilites[4];
-                    message.channel.send(res[0], {
-                        file : res[1]
-                      });
-              }  else if ( p == 'KRYSTHALIA') {
-                    let res = possibilites[7];
-                    message.channel.send(res[0], {
-                        file : res[1]
-                      });
-              } else if ( p == 'AIKO-PROTO') {
-                    let res = possibilites[5];
-                    message.channel.send(res[0], {
-                        file : res[1]
-                      });
-              } else {
-                 let alea = parseInt(Math.random() * 9);
-                 let res = possibilites[alea];
-                 message.channel.send(res[0], {
-                    file : res[1]
-                  });
-              }
-        });
+        
 
     },
 };
+
+function verifArg(possibilites, entree) {
+  let res;
+  
+  if( entree == 'LEA' || entree == 'KAINNALY') {
+        let alea = parseInt(Math.random() * 2);
+        res = possibilites[alea];
+  } else if ( entree == 'NICO' || entree == 'NICOLAS' || entree == 'KORNAKH') {
+        res = possibilites[2];
+  } else if ( entree == 'GAEL' || entree == 'ZORG') {
+        res = possibilites[3];
+  } else if ( entree == 'ELIO' || entree == 'SINISTAG') {
+        res = possibilites[4];
+  }  else if ( entree == 'OHAYO' || entree == 'KONNICHIWA' || entree == 'KONBAWA') {
+        res = possibilites[5];
+  } else if ( entree == 'EZ' || entree == 'SIMPLE' || entree == 'BASIQUE') {
+        res = possibilites[6];
+  } else if ( entree == 'AG' || entree == 'KRYSTHALIA' || entree == 'ANNE-GAELLE') {
+        res = possibilites[7];
+  } else if ( entree == 'AIKO-PROTO') {
+        res = possibilites[5];
+  } else {
+     let alea = parseInt(Math.random() * possibilites.length);
+     res = possibilites[alea];
+  }
+  
+  message.channel.send(res[0], {
+        file : res[1]
+  });
+
+}
