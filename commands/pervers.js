53a62b1fb883379c609b367a68116a1e2672d1c5
Krysthalia <38167520+Krysthalia@users.noreply.github.com>
2018-04-25 15:41:24 +0200

    Create pervers.js

---
+++ commands/pervers.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: 'pervers',
+    aliases: ['lewd', 'smirk', 'lenny', 'wut', 'regard'],
+    usage: '',
+    description: '( ͡° ͜ʖ ͡°)',
+    execute(message, args) {
+   
+		    message.channel.send('', {
+            file : "https://raw.githubusercontent.com/Krysthalia/aiko-proto/master/img/aiko-lewd.gif"
+        });
+
+        
+    },
+};
