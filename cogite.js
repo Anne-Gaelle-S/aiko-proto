7365f40027c6474cfa5b1beb31c8a647824dc443
Krysthalia <38167520+Krysthalia@users.noreply.github.com>
2018-09-19 11:29:07 +0200

    Update cogite.js

--- cogite.js
+++ cogite.js
@@ -74,8 +74,8 @@ function timeQuestion (msg){
 	
 	motifs.forEach(function(word) { 
 		console.log(word);
-	   	if(word.toUpperCase == "QUAND"){ res++; console.log("res++"+res); }
-		if(word == '?'){ res++; console.log("res++"+res); }
+	   	if(word.toUpperCase == "QUAND"){ res++; console.log("res++ quand"+res); }
+		if(word == '?'){ res++; console.log("res++ ?"+res); }
 	});
 	
 	console.log("res: "+res);
