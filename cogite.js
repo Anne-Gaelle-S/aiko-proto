2b373be628440ec7d15ecd153a0530ae4b54885c
Anne-Gaëlle Schall <anne.gaelle.schall@eisti.fr>
2018-10-01 16:50:33 +0200

    schall

--- cogite.js
+++ cogite.js
@@ -92,7 +92,7 @@ function commandesAPart(message, mutes){
 		case 1:
 			mutes[0]=0;
 			if(auteur == "KRYSTHALIA" || auteur=="KAINNALY"){
-				if(taggedUser=="KRYSTHALIA"){
+				if(taggedUser.username=="KRYSTHALIA"){
 					message.channel.send("YOU SCHALL NOT MUTE !");
 				} else {
 					message.channel.send(taggedUser+", tu parles trop, tais toi un peu.");
@@ -103,7 +103,7 @@ function commandesAPart(message, mutes){
 		case 2:
 			mutes[0]=0;
 			if(auteur == "KRYSTHALIA" || auteur=="KAINNALY"){
-				if(taggedUser=="KRYSTHALIA"){
+				if(taggedUser.username=="KRYSTHALIA"){
 					message.channel.send("YOU SCHALL NOT UNMUTE !");
 				} else {
 					message.channel.send(taggedUser+", tu peux à nouveau parler.");
