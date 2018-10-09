445922725c052e35dfdac093869656bac61486a3
Krysthalia <38167520+Krysthalia@users.noreply.github.com>
2018-10-09 17:07:21 +0200

    Update amazing.js

--- commands/amazing.js
+++ commands/amazing.js
@@ -2,10 +2,10 @@ module.exports = {
     name: 'e',
     aliases: ['amazing', 'maze', 'labyrinth', 'laby'],
     usage: ``,
-    description: 'MAZING !!',
+    description: 'C\'est deux labyrinthes côtes à côtes, qu\'est ce que tu voyais ?',
     execute(message, args) {
 
-		message.channel.send('', {
+		message.channel.send('MAZING !!', {
             file : "https://raw.githubusercontent.com/Krysthalia/aiko-proto/master/img/mazing.png"
         });
     },
