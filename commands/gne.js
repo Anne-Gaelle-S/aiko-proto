0a25b4f741a6212dd68c00efaeb4aa2f3fbb8508
Krysthalia <38167520+Krysthalia@users.noreply.github.com>
2018-10-15 15:35:50 +0200

    Create gne.js

---
+++ commands/gne.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+    name: 'gne',
+    aliases: ['gné'],
+    usage: ``,
+    description: 'GNEEEEEEEEEEEEEEEEEEEEEEEEEEE',
+    execute(message, args) {
+
+		message.channel.send('Gné.', {
+            file : "https://raw.githubusercontent.com/Krysthalia/aiko-proto/master/img/gneXeljira.png"
+        });
+
+    },
+};
